c764e9b57831cf4d7529ff02bc0ee67f03568341
mmcrhyme <masato23.maruyama@gmail.com>
2023-06-20 18:11:38 +0900

    Tweetテーブルからデータを取り出して表示までを実装

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
+// 以下を追加
+use App\Models\Tweet;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
@@ -14,11 +16,21 @@ class IndexController extends Controller
     {
         //return view('view先のディレクトリ.ファイル名')
         // -with('変数', '値');
-        // 変数に値を持ってファイル先に遷移
+        // 遷移先の変数に値（このファイル内で定義した変数）を持ってファイル先に遷移
 
-        return view('tweet.index')
-            ->with('name', 'Laravel')
-            ->with('version', '8');
+        // return view('tweet.index')
+        //     ->with('name', 'Laravel')
+        //     ->with('version', '8');
+
+        // 第二弾として以下を追加
+        $tweets = Tweet::all();
+        // dd($tweets);
+        //Tweetモデルからtweetsテーブルのデータを全て取り出して$tweetsにいれる
+        //dd(変数)はver_dumpみたいなもん。
+
+        return view ('tweet.index')
+            ->with('tweets', $tweets);
+        
 
     }
 }
--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -10,8 +10,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __($name) }}
-                    {{ __($version) }}
+                    @foreach($tweets as $tweet)
+                        <p>{{ 'タイトル：' }}{{ $tweet -> title }}</p><br>
+                        <p>{{ '問題：' }}{{ $tweet -> problem }}</p><br>
+                        <p>{{ '解決法：' }}{{ $tweet -> solution }}</p><br>
+                        <br>
+                    @endforeach
                 </div>
             </div>
         </div>

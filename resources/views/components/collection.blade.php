899ebeac4367dc7d3152a01a6f726b41d7a7dc99
mmcrhyme <masato23.maruyama@gmail.com>
2023-06-22 08:41:14 +0900

    ロゴの変更、ハンバーガーメニューにTimelineの追加、ツイートのコンポーネント化

---
+++ resources/views/components/collection.blade.php
@@ -0,0 +1,4 @@
+<div class="flex justify-between p-4 m-3 items-center bg-blue-900 text-white rounded-lg border-2 border-white">
+    <div>{{ $slot }}</div>
+    <div>{{ $editdelete }}</div>
+  </div>
